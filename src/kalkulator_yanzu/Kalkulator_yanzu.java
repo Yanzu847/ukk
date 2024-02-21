6aeb84c0403958da8e3b35f5a40e731992babd11
zulkh <zulkh@192.168.10.130>
2024-02-21 13:20:35 +0700

    my commit

---
+++ src/kalkulator_yanzu/Kalkulator_yanzu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator_yanzu;
+
+/**
+ *
+ * @author zulkh
+ */
+public class Kalkulator_yanzu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
